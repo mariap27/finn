74247a29b302aa8c9138a3f36a63ca4700cd2967
candytoy <43295976+mariap27@users.noreply.github.com>
2022-10-27 00:36:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let rainbow;
+
+rainbow = document.querySelector('.rainbow');
+
+function goToRainbow(){
+  document.location.href = 'rainbow.html'
+}
+
+rainbow.addEventListener('click', goToRainbow);
